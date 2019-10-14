57991cfe4da142dce28bfa0a1b9e60b9875fec34
Jacob Hein <heinj82@uwosh.edu>
2019-10-14 16:56:35 -0500

    Added content to CreateItem/DeleteItem and made a GetAllPLUs method.

---
+++ IInventoryDataAccess.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalProject1
+{
+    public interface IInventoryDataAccess
+    {
+        bool CreateItem(Item item);
+
+        bool UpdateItem(string pLU, Item item);
+
+        bool DeleteItem(string pLU);
+
+        Item GetItem(string pLU);
+
+        List<Item> GetAllItems();
+    }
+}
---
+++ InventoryController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalProject1
+{
+    class InventoryController : IInventoryController
+    {
+        public bool CreateItem(Item item)
+        {
+            InventoryDataAccess iDA = new IInventoryDataAccess();
+            if (GetItem(item.GetPLU) != item.GetPLU)
+            {
+                iDA.CreateItem(item);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            //throw new NotImplementedException();
+        }
+
+        public bool DeleteItem(string pLU)
+        {
+            InventoryDataAccess iDA = new IInventoryDataAccess();
+            if (!GetAllPLUs().Contains(pLU))
+            {
+                iDA.DeleteItem(pLU);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            //throw new NotImplementedException();
+        }
+        public List<string> GetAllPLUs()
+        {
+            throw new NotImplementedException();
+        }
+        public List<Item> GetAllItems()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Item GetItem(string iD)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateItem(string iD, Item item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
