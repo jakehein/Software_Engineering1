122c5d5bf799e6f0d67c9c36390c65b3aad775f3
Jacob Hein <heinj82@uwosh.edu>
2019-10-24 11:53:39 -0500

    work from monday (IUserController and ICartController)

---
+++ ICartController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalProject1
+{
+    interface ICartController
+    {
+        bool AddItem(ItemDTO item);
+
+        bool ChangeQuantity(int quantity, ItemDTO item);
+
+        bool RemoveItem(string uPC);
+
+        //List<string> AllUPCs { get; }
+
+        ItemDTO GetItem(string uPC);
+
+        List<ItemDTO> GetAllItems();
+
+        decimal GetTotal { get; }
+
+        void Checkout();
+
+        void DeleteCart();
+    }
+}
