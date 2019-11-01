c62426ba0a6cb112e1e8d8de6d89475f7a92243d
Jacob Hein <heinj82@uwosh.edu>
2019-10-31 22:05:35 -0500

    edited text size of total textblock and upc textbox. Also started the AddItem_Click feature. (doesn't work atm, breaks at Items level)

--- GUI/SalesPage.xaml.cs
+++ GUI/SalesPage.xaml.cs
@@ -20,11 +20,39 @@ namespace FinalProject1
     /// </summary>
     public partial class SalesPage : Page
     {
+        private IInventoryController inventoryControl;
+        private ICategoryController categoryControl;
+        private ICartController cartController;
+        private bool reading = false;
+        private List<ItemDTO> itemDTOs = new List<ItemDTO>();
         public SalesPage()
         {
             InitializeComponent();
+            categoryControl = new CategoryController(new CategoryDataAccess());
+            inventoryControl = new InventoryController(new InventoryDataAccess(new CategoryDataAccess()));
+            cartController = new CartController();
+            //UPCText.Focus();
+            PopulateItemList();
+            FillCategoryCombo();
+            FillItemList(itemDTOs);
         }
-
+        private void PopulateItemList()
+        {
+            itemDTOs = inventoryControl.GetAllItems();
+        }
+        void FillItemList(IEnumerable<ItemDTO> items)
+        {
+            Inventory.ItemsSource = items;
+        }
+        void FillCategoryCombo()
+        {
+            List<CategoryDTO> categories = categoryControl.GetAllCategories();
+            //CategoryListCombo.ItemsSource = categoryControl.GetAllCategories();
+            categories.Add(new CategoryDTO { CategoryID = 0, Items = null, Name = "All" });
+            categories.Sort();
+            Category.ItemsSource = categories;
+            Category.SelectedIndex = 0;
+        }
         private void MainMenu_Click(object sender, RoutedEventArgs e)
         {
             //this.NavigationService.Navigate(new MainMenu());
@@ -43,7 +71,17 @@ namespace FinalProject1
 
         private void AddItem_Click(object sender, RoutedEventArgs e)
         {
-
+            try
+            {
+                ItemDTO itm = (ItemDTO)Inventory.SelectedItem;
+                cartController.AddItem(itm);
+                Transaction.ItemsSource = cartController.GetAllItems();
+                
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Select an item to add to the transaction.");
+            }
         }
 
         private void QuantityUp_Click(object sender, RoutedEventArgs e)
@@ -65,5 +103,30 @@ namespace FinalProject1
         {
 
         }
+
+        private void UpC_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            ((TextBox)sender).FontSize = ((TextBox)sender).ActualHeight / 2;
+        }
+
+        private void PayTotal_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            ((TextBlock)sender).FontSize = ((TextBlock)sender).ActualHeight / 1.5;
+        }
+
+        /*private void Inventory_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ItemDTO itm = (ItemDTO)Inventory.SelectedItem;
+            if (itm != null)
+            {
+                
+                //UPCText.Text = itm.UPC;
+                //NameText.Text = itm.Name;
+                //QuantityText.Text = itm.Quantity.ToString();
+                //PriceText.Text = itm.Price.ToString();
+                //CategoryListCombo.SelectedIndex = CategoryListCombo.Items.IndexOf(Category.createDTOFromCategory(itm.Category));
+            }
+        }*/
+
     }
 }
