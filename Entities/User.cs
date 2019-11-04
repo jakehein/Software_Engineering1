db43661683e70321fd7240db451aa91e295439d8
Damon <damonsalm12@gmail.com>
2019-11-04 13:36:28 -0600

    Created User entity

---
+++ Entities/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalProject1
+{
+    class User
+    {
+        public string Username { get; set; }
+
+        public string PIN { get; set; }
+
+        public string HasInventoryAccess { get; set; }
+    }
+}
